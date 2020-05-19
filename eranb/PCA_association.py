0d4269c522237cf543c464de1e3fa5354d9eeeaf
daphna <dafna.aloni@gmail.com>
2020-05-18 23:12:26 -0700

    Simpler code

--- eranb/PCA_association.py
+++ eranb/PCA_association.py
@@ -9,32 +9,38 @@ from scipy.stats import mannwhitneyu
 import os
 from sklearn.decomposition import PCA
 
-def plot_PCA(X,Y=None, plot=True,format='pdf'):
-
+def plot_PCA(X,Y=None, impute = None,plot=True,format='pdf'):
+    if impute is not None:
+        X=X.fillna(X.mean())
     pca = PCA()
     allPCs = pca.fit_transform(X)
     all_pcoa_explained_var = pca.explained_variance_ratio_
     df_PCs=pd.DataFrame(allPCs, index=X.index)
     df_PCs.to_csv(os.path.join(config.analyses_path, 'PCA.csv'))
     if plot:
-        plt.figure()
-        if Y is not None:
-            colors = list(set(Y.dropna().values))
-            colors.sort()
-            cmap=plt.cm.get_cmap('cubehelix', len(colors))
+        for pc_1 in range(5):
+            for pc_2 in range(pc_1,5):
+                if pc_1==pc_2:
+                    continue
+                plt.figure(facecolor=(1, 1, 1))
+                if Y is not None:
+                    colors = list(set(Y.dropna().values))
+                    colors.sort()
+                    cmap=plt.cm.get_cmap('Blues')#, len(colors))
 
-            plt.scatter(df_PCs[0],df_PCs[1],c=Y.values,#dataColors,
-                        s=40,cmap=cmap)
-            ticksValues=list(set(Y.values))
-            plt.colorbar(ticks=ticksValues, label='Disease')
-            plt.clim(min(ticksValues)-0.5,max(ticksValues)+0.5)
-        else:
-            plt.scatter(df_PCs[0].values, df_PCs[1].values, c='b', s=40)
-        plt.xlabel('PCo1 %.2f'%all_pcoa_explained_var[0],fontsize=14)
-        plt.ylabel('PCo2 %.2f'%all_pcoa_explained_var[1],fontsize=14)
-        figname='PCA.%s'%(format)
-        plt.savefig(os.path.join(config.analyses_path, figname), format=format)
-        plt.close()
+                    plt.scatter(df_PCs[pc_1],df_PCs[pc_2],c=Y.values,#dataColors,
+                                s=40,cmap=cmap)
+                    ticksValues=list(set(Y.values))
+                    plt.colorbar(ticks=ticksValues, label='Disease')
+                    plt.clim(min(ticksValues)-0.5,max(ticksValues)+0.5)
+                else:
+                    plt.scatter(df_PCs[0].values, df_PCs[1].values, marker='.', c='b', s=40,alpha=0.6)
+                plt.xlabel('PC%s %.2f'%(pc_1+1,all_pcoa_explained_var[pc_1]),fontsize=14)
+                plt.ylabel('PC%s %.2f'%(pc_2+1,all_pcoa_explained_var[pc_2]),fontsize=14)
+                figname='PCA_%s_%s.%s'%(pc_1+1,pc_2+1,format)
+                plt.subplots_adjust(left=0.2)
+                plt.savefig(os.path.join(config.analyses_path, figname), format=format)
+        plt.show()
     return df_PCs, all_pcoa_explained_var
 
 
@@ -42,14 +48,15 @@ def getPhenotypes():
     pheno_df = pd.read_excel(config.phenotypes_df).set_index('Animal ID')
     for category ,pair in config.boolean_types.items():
         for key,value in pair.items():
-            pheno_df[pheno_df[category]==key] = value
+            pheno_df.loc[pheno_df[category]==key,category] = value
     return pheno_df.drop(config.y_category, axis=1), pheno_df[config.y_category]
 
 def corrlatePhenotypesWithDisease(X,Y):
     pvalues=[]
     statistics = []
     for phenotype in X.columns.values:
-            statistic, pvalue = mannwhitneyu(X.loc[Y == 0,phenotype],X.loc[Y == 1,phenotype])
+            subset_phenotype_x = X[phenotype].dropna()
+            statistic, pvalue = mannwhitneyu(subset_phenotype_x.values,Y.loc[subset_phenotype_x.index].values)
             statistics.append(statistic)
             pvalues.append(pvalue)
 
@@ -62,5 +69,5 @@ def corrlatePhenotypesWithDisease(X,Y):
 
 if __name__=='__main__':
     X,Y = getPhenotypes()
-    plot_PCA(X,Y)
+    plot_PCA(X,Y,impute='average')
     corrlatePhenotypesWithDisease(X,Y)
