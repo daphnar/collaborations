c7059f14494ea6e8d572d6d4d3e98a43dceb8f53
daphna <dafna.aloni@gmail.com>
2020-05-21 23:39:30 -0700

    Simpler code

--- eranb/time_dependent_phenotypes.py
+++ eranb/time_dependent_phenotypes.py
@@ -6,15 +6,14 @@ from eranb import config
 from eranb.PCA_association import getPhenotypes
 
 def run_ols_model(time,genotype,phenotype):
-    data = pd.DataFrame({'time':time,'genotype':genotype,'phenotype':phenotype})
+    data = pd.DataFrame({'time':time,'genotype':genotype,'phenotype':phenotype}).dropna()
     data['timeMgenotype']=data['genotype'].mul(data['time'])
-    stat = smf.ols("phenotype ~ time+genotype+timeMgenotype", data).fit()
-    stats = stat.pvalues.drop(['time','genotype[T.1]','Intercept'])
+    stat = smf.ols("phenotype ~ time+genotype+timeMgenotype-1", data).fit()
+    stats = stat.pvalues.drop(['time','genotype[0]','genotype[1]'])
     pvals_time = stat.pvalues['time']
-    pvals_genotype = stat.pvalues['genotype[T.1]']
+    pvals_genotype = min(stat.pvalues['genotype[0]'],stat.pvalues['genotype[1]'])
     pvals_timeMgenotype = min(stats)
-    return pvals_time,pvals_genotype,pvals_timeMgenotype
-
+    return stat.params, pvals_time,pvals_genotype,pvals_timeMgenotype
 
 def run_time_dependence(output_file):
     pvals_times=[]
@@ -23,15 +22,15 @@ def run_time_dependence(output_file):
     hypotheses=[]
     sheet = "Metadata_LumMFIav"
     for data, genotypes, sheetname in getPhenotypes():
-        if sheetname!=sheet:
-            continue
+        if sheetname==sheet:
+            break
 
     run_groups = [(config.time_behavioral,config.behavioural_tests),
                   (config.time_blood,config.blood_tests)]
     for time,phenotypes in run_groups:
         for phenotype in phenotypes:
             hypotheses.append("%s~%s + %s + %s"%(phenotype,time,'genotype',time +'*genotype'))
-            pvals_time,pvals_genotype,pvals_timeMgenotype=run_ols_model(data[time].values,genotypes.values,data[phenotype].values)
+            coefs,pvals_time,pvals_genotype,pvals_timeMgenotype=run_ols_model(data[time].values,genotypes.values,data[phenotype].values)
             pvals_times.append(pvals_time)
             pvals_genotypes.append(pvals_genotype)
             pvals_timeMgenotypes.append(pvals_timeMgenotype)
