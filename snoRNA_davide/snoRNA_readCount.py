f65291454fe0d4928eccbf59529f5803edd4a6dd
daphna <dafna.aloni@gmail.com>
2023-02-13 14:38:01 -0800

    Simpler code

--- snoRNA_davide/snoRNA_readCount.py
+++ snoRNA_davide/snoRNA_readCount.py
@@ -1,13 +1,15 @@
 #Data path: /oak/stanford/groups/pritch/users/daphna/snoRNA/pivus_snoRNA_data
 import matplotlib.pyplot as plt
-read_count = []
 import glob
 from Bio import SeqIO
 import gzip
-for sample in glob.glob('*R1.trimmed.fastq.gz'):
+
+read_count = []
+for sample in glob.glob('*R1*.fastq.gz'):
     with gzip.open(sample,'rt') as f_h:
         read_count.append(len(list(SeqIO.parse(f_h,'fastq'))))
+
 plt.hist(read_count,30)
 plt.xlabel('Number of snoRNA mapped reads')
 plt.ylabel('Count')
-plt.savefig('HistSnoRNAReadCount.png')
+plt.savefig('HistSnoRNAReadCount.pdf',format='pdf')
